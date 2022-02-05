38bbd945c4397287803a2ce5cb069c93f2708bfa
Gabriel Martins <martinsgabriel1956@gmail.com>
2022-02-04 22:38:26 -0300

    feat: using HTTP Methods

--- src/index.js
+++ src/index.js
@@ -2,10 +2,56 @@ const express = require('express');
 
 const app = express();
 
-app.get('/', (request, response) => {
-  response.json({
-    message: 'Hello Everyone!'
-  });
+app.get('/courses', (request, response) => {
+  response.json([
+    "Curso 1",
+    "Curso 2",
+    "Curso 3",
+  ]);
+});
+
+app.post('/courses', (request, response) => {
+  response.json([
+    "Curso 1",
+    "Curso 2",
+    "Curso 3",
+    "Curso 4",
+  ]);
+});
+
+app.post('/courses', (request, response) => {
+  response.json([
+    "Curso 1",
+    "Curso 2",
+    "Curso 3",
+    "Curso 4",
+  ]);
+});
+
+app.put('/courses/:id', (request, response) => {
+  response.json([
+    "Curso 5",
+    "Curso 6",
+    "Curso 7",
+    "Curso 8",
+  ]);
+});
+
+app.patch('/courses/:id', (request, response) => {
+  response.json([
+    "Curso 9",
+    "Curso 10",
+    "Curso 11",
+    "Curso 12",
+  ]);
+});
+
+app.delete('/courses/:id', (request, response) => {
+  response.json([
+    "Curso 9",
+    "Curso 10",
+    "Curso 12",
+  ]);
 });
 
 app.listen(3333);
